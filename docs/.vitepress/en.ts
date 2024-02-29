2d9885160dada4870fc710391e0cc5bf72dd1f51
Kiss For U <able2012@163.com>
2024-02-29 12:37:34 +0800

    feat: Tool Browser QQ

--- docs/.vitepress/en.ts
+++ docs/.vitepress/en.ts
@@ -58,7 +58,8 @@ function sidebarNavigation(): DefaultTheme.SidebarItem[] {
       items: [
         { text: 'Globe Explorer', link: 'web/globe-explorer' },
         { text: 'PicX', link: 'web/picx' },
-        { text: 'VR Wonder World', link: 'web/vr-wonder-world' }
+        { text: 'VR Wonder World', link: 'web/vr-wonder-world' },
+        { text: 'Tool Browser QQ', link: 'tool-browser-qq'}
       ]
     }
   ]
---
+++ docs/navigation/web/tool-browser-qq.md
@@ -0,0 +1,21 @@
+# Tool Browser QQ
+
+> 更新时间：2024-02-29
+
+## 说明
+
+* 腾讯出品工具箱
+* 开发者的：PDF，图片，数据转换等等
+* 产品设计：形成一个类似思维导图的结构，方便你能快速明了的查看要搜索的信息。
+* 智能语音：语音转文本，文本转语音等
+
+::: warning 注意
+根据自己的喜好选择
+:::
+
+## 体验
+
+* 图片
+![腾讯工具展示](https://github.com/kissfu/picx-images-hosting/raw/master/navigation/web/tool_browser_qq.92pvcoad87.gif)
+
+* 立即访问：https://tool.browser.qq.com/
