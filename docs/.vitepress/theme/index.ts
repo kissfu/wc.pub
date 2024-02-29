b8197ef8e6ceaa070b68e4937d6665910d69bfb3
Kiss For U <able2012@163.com>
2024-02-29 14:03:10 +0800

    feat: them

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,19 @@
-// .vitepress/theme/index.ts
+// https://vitepress.dev/guide/custom-theme
+import { h } from 'vue'
+import type { Theme } from 'vitepress'
 import DefaultTheme from "vitepress/theme"
 import googleAnalytics from 'vitepress-plugin-google-analytics'
 
 export default {
   extends: DefaultTheme,
-  enhanceApp({app}) {
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
+  },
+  enhanceApp({app, router, siteData}) {
     googleAnalytics({
       id: 'G-R0528T9ZQW', //跟踪ID，在analytics.google.com注册即可
     })
   }
-}
+} satisfies Theme
