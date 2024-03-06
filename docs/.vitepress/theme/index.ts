5f4ccfd6e848e0c2197828bced741df9029703b8
Kiss For U <able2012@163.com>
2024-03-06 12:19:38 +0800

    feat: image zoom

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,9 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
 import DefaultTheme from "vitepress/theme"
+import './style/index.css' 
+import { h, onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
+import mediumZoom from 'medium-zoom'
 import googleAnalytics from 'vitepress-plugin-google-analytics'
 
 export default {
@@ -15,5 +17,19 @@ export default {
     googleAnalytics({
       id: 'G-R0528T9ZQW', //跟踪ID，在analytics.google.com注册即可
     })
+  },
+  setup() {
+    const route = useRoute()
+    const initZoom = () => {
+      // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
   }
-} satisfies Theme
+}
